5eafa462c45f417bc60e7e4a95bf0db548c8ca24
John Doerfler <johndoerfler@gmail.com>
2017-01-20 14:39:37 +0700

    added proper sizes/basic css for DockPicker and DockView so they fill the whole screen and resize properly on differet devices

--- app/components/AppContainer.jsx
+++ app/components/AppContainer.jsx
@@ -9,15 +9,16 @@ class AppContainer extends React.Component {
     return (
       <div>
         <Navbar/>
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-xs-8 col-sm-9 col-lg-10">
+        <div className="container-fluid height-100">
+          <div className="row height-100">
+            <div className="col-xs-8 col-sm-9 col-lg-10 dock-view-column">
               <DockView/>
             </div>
-            <div className="col-xs-4 col-sm-3 col-lg-2">
+            <div className="col-xs-4 col-sm-3 col-lg-2 dock-picker-column">
               <DockPicker/>
             </div>
           </div>
+
         </div>
       </div>
     )
