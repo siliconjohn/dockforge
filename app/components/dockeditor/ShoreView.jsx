05b9ae0061440cae8676b6ee6db6e7c68c54517b
John Doerfler <johndoerfler@gmail.com>
2017-01-21 14:36:06 +0700

    added shoreview

---
+++ app/components/dockeditor/ShoreView.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ShoreView extends React.Component {
+
+  render() {
+    return (
+      <div className="shore-view">
+      </div>
+    )
+  }
+}
+
+module.exports = ShoreView
