4aaf1ca99d3885c1730711d38e5ca1940d765a42
John Doerfler <johndoerfler@gmail.com>
2017-01-27 09:27:23 +0700

    added basic drag and drop for creating new components

--- app/components/dockeditor/DockComponent.jsx
+++ app/components/dockeditor/DockComponent.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 class DockComponent extends React.Component {
 
+  constructor( props ) {
+    super( props )
+
+    // bind onDragStart so props can be accessed
+    this.onDrop = this.onDrop.bind( this )
+    this.onDragOver = this.onDragOver.bind( this )
+  }
+
+  onDrop( event ) {
+    let data = JSON.parse( event.dataTransfer.getData( "text" ))
+
+    // new component can't be dropped here ( for now )
+    if( data.source == "newComponent") {
+      event.preventDefault()
+      event.stopPropagation()
+    }
+  }
+
+  onDragOver( event ) {
+    event.stopPropagation()
+  }
+
   render() {
     let { left, bottom, width, height } = this.props
 
@@ -13,7 +35,8 @@ class DockComponent extends React.Component {
       height: `${height}px`,
     }
     return (
-      <div className="dock-component" style={ style }></div>
+      <div className="dock-component" style={ style } onDrop={ this.onDrop }
+        onDragOver={ this.onDragOver }></div>
     )
   }
 }
--- app/components/dockeditor/DockView.jsx
+++ app/components/dockeditor/DockView.jsx
@@ -1,14 +1,48 @@
 import React from 'react'
-import { connect } from 'react-redux'
+import { connect, dispatch } from 'react-redux'
 import DockComponent from 'DockComponent'
+import { addDockComponent } from 'actions'
 
 class DockView extends React.Component {
+
+  constructor( props ) {
+    super( props )
+
+    // bind onDragStart so props can be accessed
+    this.onDrop = this.onDrop.bind( this )
+    this.onDragOver = this.onDragOver.bind( this )
+  }
+
+  onDrop( event ) {
+    event.preventDefault()
+
+    let data = JSON.parse( event.dataTransfer.getData( "text" ))
+
+    // get mouse's x y drop position
+    let elementRect = event.currentTarget.getBoundingClientRect()
+    let dropMouseX = event.clientX - elementRect.left
+    let dropMouseY = event.clientY - elementRect.top
+
+    // create the component
+    var newComponent = { type: data.type, left:dropMouseX,
+      bottom: elementRect.height - dropMouseY - data.length,
+      width: data.width, height:data.length }
+
+    // dispatch event adding new component
+    this.props.dispatch( addDockComponent( newComponent ))
+  }
+
+  onDragOver( event ) {
+    event.preventDefault()
+    event.dataTransfer.dropEffect = "copy"
+  }
 
   render() {
     var { dock } = this.props
 
     return (
-      <div className="dock-view">
+      <div className="dock-view" onDrop={ this.onDrop }
+        onDragOver={ this.onDragOver }>
         {
           dock.map(( item, index ) => {
             return <DockComponent { ...item } key={ index }/>
@@ -20,11 +54,13 @@ class DockView extends React.Component {
 }
 
 DockView.propTypes = {
-  dock: React.PropTypes.array.isRequired
+  dock: React.PropTypes.array.isRequired,
+  dispatch: React.PropTypes.func.isRequired,
 }
 
 export default connect (( state ) => {
   return {
-    dock: state.dock
+    dock: state.dock,
+    components: state.components
   }
 })( DockView )
