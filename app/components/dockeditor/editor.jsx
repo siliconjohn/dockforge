76d90ad6f280f9208051844dd12965b163b293eb
John Doerfler <johndoerfler@gmail.com>
2017-02-09 20:02:54 +0700

    reformated

--- app/components/dockeditor/editor.jsx
+++ app/components/dockeditor/editor.jsx
@@ -2,16 +2,15 @@ import React from 'react'
 import Square from 'Square'
 import Rectangle from 'Rectangle'
 
-// returns the proper component giving the attr.type
-
 // When you add a new component it needs to be added
 // to this object to be available to the app
 const components = {
   Square: Square,
   Rectangle: Rectangle
-};
+}
 
+// returns the proper component giving the attr.type
 module.exports.getComponent = ( attr ) => {
-  let SpecificComponent = components[attr.type]
+  let SpecificComponent = components[ attr.type ]
   return <SpecificComponent { ...attr } key={ attr.uuid } />
 }
