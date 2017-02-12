df9a38ecbb7b7ae0f05fcee51d626f2f4595e2f2
John Doerfler <johndoerfler@gmail.com>
2017-02-12 16:52:47 +0700

    updated to webpack 2, updated related packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,153 +1,84 @@
-var webpack = require('webpack');
-var WebpackErrorNotificationPlugin = require('webpack-error-notification');
-var WebpackNotifierPlugin = require('webpack-notifier');
-var path = require('path');
+const path = require('path');
+const webpack = require('webpack');
+// const WebpackErrorNotificationPlugin = require('webpack-error-notification');
+// const WebpackNotifierPlugin = require('webpack-notifier');
 
-var exportValues = {}
+module.exports = {
+  context: path.resolve( __dirname, './app' ),
 
-// if -p (production)
-const production = process.argv.indexOf("-p") != -1 ? true : false
+  entry: {
+    app: [
+      './app.jsx',
 
-// these files are used for both dev and production
-const entryFiles = [
-  'script!jquery/dist/jquery.min.js',
-  // all bootstrap js
-  //'script!bootstrap-sass/assets/javascripts/bootstrap.js',
-  // or individual files
-  //'script!bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
-  //'script!bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
-  './app/app.jsx'
-  ]
+      // bootstrap js
+      //'bootstrap-sass/assets/javascripts/bootstrap.js',
+      // or individual bootstrap files
+      //'bootstrap-sass/assets/javascripts/bootstrap/dropdown.js',
+      //'script!bootstrap-sass/assets/javascripts/bootstrap/collapse.js',
+    ]
+  },
 
-// these files are used for both dev and production
-const modulesDirectories = [
-    'node_modules',
-    './app/redux',
-    './app/components',
-    './app/components/dockeditor',
-    './app/components/dockeditor/componentpicker',
-    './app/components/dockeditor/svg',
-    './app/components/dockeditor/components/',
-  ]
-
-if( production ) {
-
-  /////////////////////
-  // production
-  /////////////////////
-
-  exportValues = {
-    entry: entryFiles,
-    externals: {
-      jquery: 'jQuery'
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        '$': 'jquery',
-        'jQuery': 'jquery',
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-          compress: {
-            warnings: false
-          },
-          output: {
-            comments: false,
-          },
-          minimize: true
-      }),
-      new webpack.optimize.DedupePlugin(),
-      new WebpackErrorNotificationPlugin(),
-      new WebpackNotifierPlugin({title: 'Webpack', excludeWarnings: true}),
-      new webpack.DefinePlugin({
-        'process.env':{
-          'NODE_ENV': JSON.stringify('production')
-        }
-      })
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  resolve: {
+    modules: [
+      "./app/redux",
+      "./app/components",
+      './app/components/dockeditor',
+      './app/components/dockeditor/componentpicker',
+      './app/components/dockeditor/svg',
+      './app/components/dockeditor/components',
+      "node_modules"
     ],
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js',
-      sourceMapFilename: '[file].map'
+    alias: {
+      initialState: path.resolve(__dirname, './app/initialState.jsx'),
+      applicationStyles:  path.resolve(__dirname,'./app/styles/app.scss')
     },
-    resolve: {
-      root: __dirname,
-      modulesDirectories: modulesDirectories,
-      alias: {
-        applicationStyles: 'app/styles/app.scss'
-      },
-      extensions:[ '','.js','.jsx']
-    },
-    module:{
-      loaders: [
-        {
-          loader: 'babel-loader',
-          query:{
-            presets:['react','es2015']
+    extensions: [".js", ".jsx"],
+  },
+
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery',
+    }),
+    new webpack.DefinePlugin({
+      'process.env': {
+        'NODE_ENV': JSON.stringify('development'),
+        'BABEL_ENV': JSON.stringify('dev')
+      }
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: [
+          'babel-loader',
+          {
+            loader: 'babel-loader',
+            options: { presets: ['react','es2015'] },
           },
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/
-        }
-      ]
-    },
-    sassLoader: {
-      includePaths:[
-        path.resolve(__dirname, './node_modules/bootstrap-sass/assets'),
-      ]
-    },
-    devtool: 'source-map'
-  }
-} else {
-
-  ////////////////////
-  // dev
-  ////////////////////
-
-  exportValues = {
-    entry: entryFiles,
-    externals: {
-      jquery: 'jQuery'
-    },
-    plugins: [
-      new webpack.ProvidePlugin({
-        '$': 'jquery',
-        'jQuery': 'jquery',
-      }),
-      new webpack.optimize.DedupePlugin(),
-      new WebpackErrorNotificationPlugin(),
-      new WebpackNotifierPlugin({title: 'Webpack', excludeWarnings: true})
+        ],
+      },
+      {
+              test: /\.scss$/,
+              use: [{
+                  loader: "style-loader"
+              }, {
+                  loader: "css-loader"
+              }, {
+                  loader: "sass-loader",
+                  options: {
+                      includePaths: [path.resolve(__dirname, './node_modules/bootstrap-sass/assets')]
+                  }
+              }]
+          }
     ],
-    output: {
-      path: __dirname,
-      filename: './public/bundle.js',
-      sourceMapFilename: '[file].map'
-    },
-    resolve: {
-      root: __dirname,
-      modulesDirectories: modulesDirectories,
-      alias: {
-        applicationStyles: 'app/styles/app.scss'
-      },
-      extensions:[ '','.js','.jsx']
-    },
-    module:{
-      loaders: [
-        {
-          loader: 'babel-loader',
-          query:{
-            presets:['react','es2015']
-          },
-          test: /\.jsx?$/,
-          exclude: /(node_modules|bower_components)/
-        }
-      ]
-    },
-    sassLoader: {
-      includePaths:[
-        path.resolve(__dirname, './node_modules/bootstrap-sass/assets'),
-      ]
-    },
-    devtool: 'cheap-source-map'
-  }
+  },
+  devtool: 'source-map'
 }
-
-module.exports = exportValues
