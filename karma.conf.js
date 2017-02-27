6687cd9833724ac3be63cb0fa3bef9fbaee36d38
John Doerfler <johndoerfler@gmail.com>
2017-02-27 12:05:06 +0700

    updated extensions to look for in tests

--- karma.conf.js
+++ karma.conf.js
@@ -2,15 +2,15 @@ var webpackConfig = require('./webpack.config.js');
 
 module.exports = function (config) {
   config.set({
-    browsers: ['Chrome'],  
+    browsers: ['Chrome'],
     singleRun: true,
     frameworks: ['mocha'],
     files: [
       'node_modules/jquery/dist/jquery.min.js',
-      'app/tests/**/*.test.jsx'
+      'app/tests/**/*.test.js'
     ],
     preprocessors: {
-      'app/tests/**/*.test.jsx': ['webpack', 'sourcemap']
+      'app/tests/**/*.test.js': ['webpack', 'sourcemap']
     },
     reporters: ['mocha'],
     client: {
