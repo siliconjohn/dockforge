a992e22b4aca7416184123c301c8ae1c79cf1ff6
John Doerfler <johndoerfler@gmail.com>
2017-02-27 12:05:14 +0700

    added first test

---
+++ app/tests/app.test.js
@@ -0,0 +1,12 @@
+var React = require('react');
+var expect = require('expect');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+
+describe('App', () => {
+
+  it('should exist', () => {
+    expect(1).toBe(1);
+  });
+
+});
