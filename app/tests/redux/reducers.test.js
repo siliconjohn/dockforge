894b3e71a82094af339bfda52cb20f05e8421c41
John Doerfler <johndoerfler@gmail.com>
2017-02-28 20:14:22 +0700

    added readOnly reducer test

--- app/tests/redux/reducers.test.js
+++ app/tests/redux/reducers.test.js
@@ -1,37 +1,29 @@
-// import * as React from 'react'
-// import * as expect from 'expect'
-// import * as actions from '../../redux/actions'
-//
-// import * as reducers from '../../redux/reducers'
-// import * as deepFreeze from 'deep-freeze-strict'
-//
-// const testState = {
-//
-//           svgWidth: 600,
-//           svgHeight: 600,
-//           svgScale: 0.8,
-//           svgRotation: 0,
-//           svgShorelineHeight: 75,
-//           svgShowGrid: true,
-//           svgShowCenterLine: true,
-//           svgShowDistances: true,
-//           readOnly: false,
-//
-//   }
-//
-// describe( 'Redux Reducers', () => {
-//
-//   describe( ' ', () => {
-//
-//     it( 'Should change  ', () => {
-//       var action = {
-//         type: actions.TOGGLE_READ_ONLY
-//       }
-//
-//       let initialState = JSON.parse( JSON.stringify( testState ))
-//
-//       var response = reducers.updateDockComponent( deepFreeze( initialState ), deepFreeze( action ))
-//       expect( response.readOnly ).toEqual( !initialState.readOnly )
-//     })
-//   })
-// })
+var expect = require('expect')
+var reducers = require('reducers')
+var actions = require('actions')
+var deepFreeze = require('deep-freeze-strict')
+
+const testState = {
+    svgWidth: 600,
+    svgHeight: 600,
+    svgScale: 0.8,
+    svgRotation: 0,
+    svgShorelineHeight: 75,
+    svgShowGrid: true,
+    svgShowCenterLine: true,
+    svgShowDistances: true,
+    readOnly: false,
+  }
+
+describe( 'Redux Reducers', () => {
+
+  it( 'Should toggle readOnly  ', () => {
+    var action = {
+      type: actions.TOGGLE_READ_ONLY
+    }
+
+    let initialState = JSON.parse( JSON.stringify( testState ))
+    var response = reducers.updateDockComponent( deepFreeze( initialState ), action )
+    expect( response.readOnly ).toEqual( !initialState.readOnly )
+  })
+})
