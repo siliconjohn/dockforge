792cf07c7d2a0cd9e8b2f7c6e871fd77220a82a4
John Doerfler <johndoerfler@gmail.com>
2017-03-01 19:03:27 +0700

    moved global state reducers to 'reducersGlobal' file

--- app/redux/reducers.js
+++ app/redux/reducers.js
@@ -1,54 +1,14 @@
 import { combineReducers } from 'redux'
-import * as actions from 'actions'
 import * as reducersDock from 'reducersDock'
-
-export var setDraggingComponent = ( state = null, action ) => {
-  if ( action.type == actions.SET_DRAG_COMPONENT ) {
-    return action.component
-  } else {
-    return state
-  }
-}
-
-export var setMouseMoveXY = ( state = [0,0], action ) => {
-  if ( action.type == actions.SET_MOUSE_MOVE_XY ) {
-    return action.value
-  } else {
-    return state
-  }
-}
-
-export var setMouseDraggingElement = ( state = false, action ) => {
-  if ( action.type == actions.SET_MOUSE_DRAGGING_ELEMENT ) {
-    return action.value
-  } else {
-    return state
-  }
-}
-
-export var setTouchMoveXY = ( state = [0,0], action ) => {
-  if ( action.type == actions.SET_TOUCH_MOVE_XY ) {
-    return action.value
-  } else {
-    return state
-  }
-}
-
-export var setTouchDraggingElement = ( state = false, action ) => {
-  if ( action.type == actions.SET_TOUCH_DRAGGING_ELEMENT ) {
-    return action.value
-  } else {
-    return state
-  }
-}
+import * as reducersGlobal from 'reducersGlobal'
 
 export const appReducers = combineReducers({
-  dock: dockReducers,
-  draggingComponent: setDraggingComponent,
-  mouseMoveXY: setMouseMoveXY,
-  mouseDraggingElement: setMouseDraggingElement,
-  touchMoveXY: setTouchMoveXY,
-  touchDraggingElement: setTouchDraggingElement,
+  dock: reducersDock.updateDockComponent,
+  draggingComponent: reducersGlobal.setDraggingComponent,
+  mouseMoveXY: reducersGlobal.setMouseMoveXY,
+  mouseDraggingElement: reducersGlobal.setMouseDraggingElement,
+  touchMoveXY: reducersGlobal.setTouchMoveXY,
+  touchDraggingElement: reducersGlobal.setTouchDraggingElement,
   newDock: ( state = {} ) => state,
   svgShowDistances: (state = {}) => state,
   svgShorelineHeight: (state = {}) => state,
---
+++ app/redux/reducersGlobal.js
@@ -0,0 +1,41 @@
+import * as actions from 'actions'
+
+export var setDraggingComponent = ( state = null, action ) => {
+  if ( action.type == actions.SET_DRAG_COMPONENT ) {
+    return action.component
+  } else {
+    return state
+  }
+}
+
+export var setMouseMoveXY = ( state = [0,0], action ) => {
+  if ( action.type == actions.SET_MOUSE_MOVE_XY ) {
+    return action.value
+  } else {
+    return state
+  }
+}
+
+export var setMouseDraggingElement = ( state = false, action ) => {
+  if ( action.type == actions.SET_MOUSE_DRAGGING_ELEMENT ) {
+    return action.value
+  } else {
+    return state
+  }
+}
+
+export var setTouchMoveXY = ( state = [0,0], action ) => {
+  if ( action.type == actions.SET_TOUCH_MOVE_XY ) {
+    return action.value
+  } else {
+    return state
+  }
+}
+
+export var setTouchDraggingElement = ( state = false, action ) => {
+  if ( action.type == actions.SET_TOUCH_DRAGGING_ELEMENT ) {
+    return action.value
+  } else {
+    return state
+  }
+}
