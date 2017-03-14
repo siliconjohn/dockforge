98fd0fbe61ba79d6496454d6960e8cd409ed9709
John Doerfler <johndoerfler@gmail.com>
2017-03-14 14:25:51 +0700

    added <Highlight/> component

--- app/components/dockeditor/components/Rectangle.jsx
+++ app/components/dockeditor/components/Rectangle.jsx
@@ -1,25 +1,16 @@
 import React from 'react'
+import Highlight from 'Highlight'
 
 var Rectangle = ( props ) => {
-
-  let { left, bottom, width, height, uuid, draggingOver } = props
-
-  const getHightlight = () => {
-    if( draggingOver == true ) {
-      return (
-        <rect stroke="red" strokeWidth="4" fill="none"
-          x={ left } y={ bottom - height } width={ width } height= { height }/>
-      )
-    } 
-  }
+  let { left, bottom, width, height, uuid, draggingOver, draggingOverSide } = props
 
   return (
-    <g className="rectangle" data-uuid={ uuid } >
-      {  getHightlight() }
+    <g className="rectangle" data-uuid={ uuid } > 
       <rect stroke="darkblue" strokeWidth="1" fill="red" data-uuid={ uuid }
         x={ left } y={ bottom - height / 2 } width={ width } height= { height / 2 }/>
       <rect stroke="darkblue" strokeWidth="1" fill="yellow" data-uuid={ uuid }
         x={ left } y={ bottom - height  } width={ width } height= { height / 2 }/>
+      <Highlight { ...props }/>
     </g>
   )
 }
@@ -31,6 +22,7 @@ Rectangle.propTypes = {
   height: React.PropTypes.number.isRequired,
   uuid: React.PropTypes.string.isRequired,
   draggingOver: React.PropTypes.bool,
+  draggingOverSide: React.PropTypes.string,
 }
 
 module.exports = Rectangle
