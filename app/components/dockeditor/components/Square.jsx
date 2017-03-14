0dd18fbbd71c09745158fe01a7f189af4656b6fe
John Doerfler <johndoerfler@gmail.com>
2017-03-14 14:24:25 +0700

    removed old getHightlight(), replaced with <Highlight/> in previous commit

--- app/components/dockeditor/components/Square.jsx
+++ app/components/dockeditor/components/Square.jsx
@@ -5,43 +5,6 @@ var Square = ( props ) => {
 
   let { left, bottom, width, height, uuid, draggingOver, draggingOverSide } = props
 
-  const getHightlight = () => {
-
-    if( draggingOver == true) {
-      let highlightHeight = 4
-
-      switch( draggingOverSide ) {
-        case "top":
-          return  (
-            <rect stroke="red" strokeWidth="1" fill="red"
-              x={ left } y={ bottom - height } width={ width } height= { highlightHeight }/>
-          )
-        case "bottom":
-          return  (
-            <rect stroke="red" strokeWidth="1" fill="red"
-              x={ left } y={ bottom - highlightHeight } width={ width } height= { highlightHeight }/>
-          )
-        case "right":
-          return  (
-            <rect stroke="red" strokeWidth="1" fill="red"
-              x={ left + width - highlightHeight } y={ bottom - height } width={ highlightHeight }
-              height= { height }/>
-          )
-        case "left":
-          return  (
-            <rect stroke="red" strokeWidth="1" fill="red"
-              x={ left } y={ bottom - height } width={ highlightHeight }
-              height= { height }/>
-          )
-        default :
-          return  (
-            <rect stroke="red" strokeWidth={ highlightHeight } fill="none"
-             x={ left } y={ bottom - height } width={ width } height= { height }/>
-          )
-      }
-    }
-  }
-
   return (
     <g className="square" data-uuid={ uuid }>
       <rect stroke="darkblue" strokeWidth="1" fill="blue" data-uuid={ uuid }
