5ca246998bcb451aac97763ffb435c00903d00d3
John Doerfler <johndoerfler@gmail.com>
2017-03-24 14:38:39 +0700

    added lib and basic code to start joyride

--- app/components/AppContainer.jsx
+++ app/components/AppContainer.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
 import Navbar from 'Navbar'
 import DockEditor from 'DockEditor'
+import Joyride from 'react-joyride'
 
 class AppContainer extends React.Component {
+  getSteps(){
+    let steps = []
 
+    steps.push({
+      title: ".qwe",
+      text: "test",
+      selector: "#rrr",
+      position: "left",
+    })
+
+    return steps
+    }
   render() {
     return (
       <div>
         <Navbar/>
         <DockEditor/>
+          <Joyride
+         ref="joyride"
+         steps={ this.getSteps() }
+         run={true}
+         debug={true}
+
+         />
       </div>
     )
   }
